5e023d241c92f895fc43f292430ba9b5852cda2f
Mahi Prasad <mahiprasad264@gmail.com>
2022-08-29 23:47:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var randomNumber1 = Math.floor(Math.random()*6)+1;
 var randomNumber2 = Math.floor(Math.random()*6)+1;
 
 //assigning random images
-document.querySelectorAll("img")[0].setAttribute("src", `/images/dice${randomNumber1}.png`);
+document.querySelectorAll("img")[0].setAttribute("src", `./images/dice${randomNumber1}.png`);
 
-document.querySelectorAll("img")[1].setAttribute("src", `/images/dice${randomNumber2}.png`);
+document.querySelectorAll("img")[1].setAttribute("src", `./images/dice${randomNumber2}.png`);
 
 
 //conditions for result
